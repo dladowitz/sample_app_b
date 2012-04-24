68cc3e617302b79cf6bc7c7d504441a4af883e54
David Ladowitz <David@ladowitz.com>
2012-04-24 02:30:07 -0700

    Up to 8.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,41 @@
-module SessionsHelper
-end
+ 
+ module SessionsHelper
+
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    current_user = user
+  end
+
+  def signed_in?
+  	!current_user.nil?
+  end
+
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= user_from_remember_token
+  end
+
+  def sign_out
+    current_user = nil
+    cookies.delete(:remember_token)
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default) # this was in the sample code, but not the tutorial as of 8.3
+    # clear_return_to   - This is in the sample code, but broke the tests. 
+  end
+
+  private
+
+    def user_from_remember_token
+      remember_token = cookies[:remember_token]
+      User.find_by_remember_token(remember_token) unless remember_token.nil?
+    end
+  
+  
+
+  end
