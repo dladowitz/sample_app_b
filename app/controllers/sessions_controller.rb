08614e3b5c442d24f0f7ed27bbbd7d39677113fb
David <david@aronsontech.com>
2012-05-02 18:39:04 -0700

    End of Chapter 10!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
 
-	def new
-	end
+  def new
+  end
 
-	def create
+  def create
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       sign_in user
@@ -14,9 +14,8 @@ class SessionsController < ApplicationController
     end
   end
 
-	def destroy
+  def destroy
     sign_out
     redirect_to root_path
-	end
-
+  end
 end
